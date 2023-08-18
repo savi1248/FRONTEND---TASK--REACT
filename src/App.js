395daa66947b324d51f19f1cfa00b5252916b1e2
savitha A <80628303+savi1248@users.noreply.github.com>
2023-08-18 17:38:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 
 import './App.css';
+import Home  from './Components/Home';
+import About from './Components/About'
+import Blog from './Components/Blog';
+import Contactus from "./Components/Contact us";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 
 // const bearsPicture = new URL ("./images/img1.jpg",import.meta.url)
@@ -7,7 +14,16 @@ function App() {
   
   return <>
    
-    <p>hello</p>
+   <BrowserRouter>
+   <Routes>
+   <Route>
+       <Route index element={<Home />} />
+       <Route path="About" element={<About />} />
+       <Route path="contactus" element={<Contactus />} />
+       <Route path="blog" element={<Blog/>} />
+     </Route>
+   </Routes>
+   </BrowserRouter>
      
      </>
 }
