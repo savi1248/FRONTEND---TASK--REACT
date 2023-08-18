aed3dc007cb8ffef25f0d121fddcc3ead46a6ccd
savitha A <80628303+savi1248@users.noreply.github.com>
2023-08-18 19:36:52 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,10 +8,10 @@ const Header = () => {
          </div>
          <nav className="navigation">
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">About</a></li>
-                <li><a href="/">Blogs</a></li>
-                <li><a href="/">Contact us</a></li>
+                <li><a href="/#">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/blog">Blogs</a></li>
+                <li><a href="/contactus">Contact us</a></li>
             </ul>
 
          </nav>
